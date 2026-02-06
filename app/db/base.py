178d281853483aa6763196b8c6788f13d351b86e
salah0533 <salahux863@gmail.com>
2026-02-06 16:38:29 +0100

    Add database models to base.py and create .env-example for database configuration

--- app/db/base.py
+++ app/db/base.py
@@ -1,4 +1,14 @@
 from sqlalchemy.orm import DeclarativeBase
+from app.models.employees import Employees
+from app.models.attendence import Attendence
+from app.models.attendence_types import AttendenceTypes
+from app.models.payment_types import PaymentTypes
+from app.models.payments import Payments
+from app.models.salaryType import SalaryType
+from app.models.settings import Settings
+from app.models.vacation import Vacation
+from app.models.vacation_types import VacationTypes
+from app.models.vacation_status import VacationStatus
 
 
 class Base(DeclarativeBase):
